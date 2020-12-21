4ec8eecd81688a9b5849dbe0830d435f74cd5e1d
newbeeguy <mskjavascript@gmail.com>
2020-12-21 20:22:08 +0300

    crist

--- src/components/Header/HeaderBottom/HeaderBottomNav/HeaderBottomNav.tsx
+++ src/components/Header/HeaderBottom/HeaderBottomNav/HeaderBottomNav.tsx
@@ -3,11 +3,14 @@ import classes from './HeaderBottomNav.module.scss';
 import Heart from "../../../../assets/Heart/Heart";
 import Basket from "../../../../assets/Basket/Basket";
 import Person from "../../../../assets/Person/Person";
+import House from "../../../../assets/House/House";
 
 const HeaderBottomNav = () => {
     return (
 
             <ul className={classes.menuIcon}>
+                <li className={classes.iconItems}><House/>
+                    Главная</li>
                 <li className={classes.iconItems}><Person/>
                 Войти</li>
                 <li className={classes.iconItems}><Heart/>
--- src/components/Sidebar/SidebarLogo/SidebarLogo.tsx
+++ src/components/Sidebar/SidebarLogo/SidebarLogo.tsx
@@ -19,7 +19,9 @@ const SidebarLogo:React.FC<SidebarLogoType> = ({collapsed,setCollapsed}) => {
             <div className={classes.headerLogo__icon}>
 
                     <Logo />
-
+                    <div className={classes.image}>
+                        <img src="https://storage.googleapis.com/multi-static-content/previews/artage-io-thumb-2714ff4d6b03b0c764b5df452bf906cb.png" alt="#"/>
+                    </div>
 
 
             </div>
