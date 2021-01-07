5ff6179b2e1108a1321d125dad458ab22f793386
newbeeguy <mskjavascript@gmail.com>
2021-01-07 23:07:59 +0300

    asd

--- src/components/Content/SliderShow/SliderShow.tsx
+++ src/components/Content/SliderShow/SliderShow.tsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import classes from './SliderShow.module.scss';
 import slider1Img from '../../../assets/k1.jpg';
-
+import slider2Img from '../../../assets/k2.jpg';
 import slider3Img from '../../../assets/k3.jpg';
 import slider4Img from '../../../assets/k4.jpg';
 import slider5Img from '../../../assets/k5.jpg';
+import slider6Img from '../../../assets/k6.jpg';
 import ArrowLeft from "../../../assets/ArrowLeftIcon/ArrowLeftIcon";
 import ArrowRight from "../../../assets/ArrowRightIcon/ArrowRightIcon";
 
@@ -15,16 +16,19 @@ type ImageStateType = {
 }
 const imageState: Array<ImageStateType> = [
 
-    {id: 0, img: slider3Img,},
-    {id: 1, img: slider4Img,},
-    {id: 2, img: slider1Img,},
-    {id: 3, img: slider5Img,},
+
+    {id: 1, img: slider1Img,},
+    {id: 2, img: slider2Img,},
+    {id: 3, img: slider3Img,},
+    {id: 4, img: slider4Img,},
+    {id: 5, img: slider5Img,},
+    {id: 6, img: slider6Img,},
 ]
 
 const SliderShow: React.FC<SliderShowPropsType> = () => {
 
 
-    const [slide, setSlide] = useState<number>(2);
+    const [slide, setSlide] = useState<number>(0);
 
 
     const incrementSlide = () => {
@@ -50,7 +54,7 @@ const SliderShow: React.FC<SliderShowPropsType> = () => {
             )
 
 
-        }, 5000)
+        }, 4000)
         return () => clearInterval();
     }, [])
 
