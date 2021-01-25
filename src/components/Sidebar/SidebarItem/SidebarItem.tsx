f5bf900e673248199e5588342b99623d83f1ffd1
newbeeguy <mskjavascript@gmail.com>
2021-01-25 13:29:43 +0300

    delay

--- src/components/Sidebar/SidebarItem/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem/SidebarItem.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from './SidebarItem.module.scss'
 import {DropDownMenuType, SidebarItemType} from "../../../types/types";
 import Drill from "../../../assets/Drill/Drill";
